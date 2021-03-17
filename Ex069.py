722c9e07ec6275c0368ff620dff6769de48c3912
Dionney Saraiva <dsaraiva@terra.com.br>
2021-03-17 09:45:19 -0300

    Add até Ex070

--- Ex069.py
+++ Ex069.py
@@ -1,1 +1,14 @@
-
+continua = 'S'
+maior18 = mulheres = homens = 0
+while continua in 'Ss':
+    idade = int(input('Qual a sua idade? '))
+    if idade > 18:
+        maior18 += 1
+    sexo = input('Qual o seu sexo?[M/F] ').upper().strip()[0]
+    if sexo == 'F' and idade < 20:
+        mulheres += 1
+    if sexo == 'M':
+        homens += 1
+    continua = input('Deseja continuar cadastrando?[S/N] ').upper().strip()[0]
+    print('=-' * 20)
+print('Foram cadastrados {} homens; {} pessoas têm mais de 18 anos e {} mulheres abaixo de 20 anos'.format(homens, maior18, mulheres))
--- Ex070.py
+++ Ex070.py
@@ -1,10 +1,21 @@
 continua = 'S'
 total = cont = 0
+menor = 0
+item = 0
+nome = ''
 while continua in 'Ss':
     produto = str(input('Nome do produto: '))
+    item += 1
     preco = float(input('Preço: R$ '))
+    if item == 1:
+        menor = preco
+        nome = produto
+    elif menor > preco:
+        menor = preco
+        nome = produto
     continua = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
     total += preco
     if preco > 1000:
         cont += 1
-print('O total da compra foi R$ {:.2f}. Você comprou {} produtos acima de R$ 1000,00'.format(total, cont))
+print('Você gastou R${:.2f}, sendo {} com valor acima de R$ 1000,00'.format(total, cont))
+print('O produto mais barato foi {} custando R$ {:.2f}'.format(nome, menor))
