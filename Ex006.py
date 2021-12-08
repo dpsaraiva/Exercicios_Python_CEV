d272a86e26354ccb8f983bc9812420b95350645d
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-08 15:53:54 -0300

    Update Ex006.py
    Adicionado escopo do exercício.

--- Ex006.py
+++ Ex006.py
@@ -1,2 +1,4 @@
+#Desafio: Crie um algoritmo que leia um número e mostre o seu dobro.
+
 n = int(input('Digite um número inteiro: '))
 print('Seu dobro é {}{}\033[m, seu triplo é {}{}\033[m e sua raiz quadrada é {:.2f}'.format('\033[1;34m', n*2, '\033[1;36m', n*3, n**(1/2)))
