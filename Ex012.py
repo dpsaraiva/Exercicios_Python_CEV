c5a21f5baae8713307fc8be12d3be005662c2256
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-08 16:07:42 -0300

    Update Ex012.py
    Adicionado escopo do exercício.

--- Ex012.py
+++ Ex012.py
@@ -1,2 +1,4 @@
+#Desafio: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
+
 n = float(input('Digite o preço do produto: '))
 print('Com 5% de desconto seu preço será R$ {:.2f}'.format(n*0.95))
