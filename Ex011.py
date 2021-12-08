7cc030808880e9c4591ca3621dd73d38e12f2b1a
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-08 16:06:24 -0300

    Update Ex011.py
    Adicionado escopo do exercício.

--- Ex011.py
+++ Ex011.py
@@ -1,3 +1,7 @@
+#Desafio: Faça um programa que leia a altura e a largura de uma parede em metros, calcule
+#a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta
+#pinta uma área de 2m².
+
 n1 = float(input('Qual a altura da parede em metros? '))
 n2 = float(input('Qual a largura da parede em metros? '))
 area = n1*n2
