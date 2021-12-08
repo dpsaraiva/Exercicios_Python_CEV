7f930a296b05e34b49253440ffe0bac553e7b1c1
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-08 16:03:49 -0300

    Update Ex010.py
    Adicionado escopo do exercício.

--- Ex010.py
+++ Ex010.py
@@ -1,2 +1,5 @@
+#Desafio: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e 
+#mostre quantos dólares ela pode comprar. Considere $1,00 = R$3,27
+
 n = float(input('Quanto dinheiro você tem na carteira? '))
 print('Você pode comprar {}{:.2f} dólares'.format('\033[1;31m', n/3.27))
