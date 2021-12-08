b9f83b7de50bec222996960f224d513d04132313
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-08 16:09:14 -0300

    Update Ex013.py
    Adicionado escopo do exercício.

--- Ex013.py
+++ Ex013.py
@@ -1,2 +1,4 @@
+#Desafio: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.
+
 n = float(input('Qual o seu salário? '))
 print('Seu novo salário é R$ {:.2f}'.format(n*1.15))
