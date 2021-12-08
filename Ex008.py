dd3365a7d06c5d038840c872cf32ed435a4dca8e
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-08 15:57:19 -0300

    Update Ex008.py
    Adicionado escopo do exercício.

--- Ex008.py
+++ Ex008.py
@@ -1,2 +1,4 @@
+#Desafio: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
 n = float(input('Digite o valor em metros: '))
 print('Em centímetros será {}{:.0f}\033[m cm\nEm milímetros será {}{:.0f}\033[m mm'.format('\033[1;41m', n*100, '\033[1;42m', n*1000))
