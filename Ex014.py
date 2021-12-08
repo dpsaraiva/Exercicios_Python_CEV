ea57d0b342ec920b7a840aa1e72357d5854a0390
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-08 16:12:54 -0300

    Update Ex014.py
    Adicionado escopo do exercício.

--- Ex014.py
+++ Ex014.py
@@ -1,3 +1,5 @@
+#Desafio: Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF.
+
 c = float(input('Informe a temperatura em Celsius (ºC)? '))
 f = c*1.8 + 32
 print("A temperatura em Fahrenheit é {:.1f}ºF".format(f))
