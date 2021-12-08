139302e8fdaecf5721be0ae0b5a01ca69890d138
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-08 15:52:32 -0300

    Update Ex005.py
    Adicionado escopo do exercício.

--- Ex005.py
+++ Ex005.py
@@ -1,3 +1,5 @@
+#Desafio: Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
+
 n = int(input('Digite um número: '))
 print('Seu antecessor é {}\nE seu sucessor é {} '.format(n-1, n+1))
 
