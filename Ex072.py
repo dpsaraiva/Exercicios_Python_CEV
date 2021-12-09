7b86c0d063aa44fefed7fae81fb429533923eb91
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:14:43 -0300

    Update Ex072.py
    Adicionado escopo do exercício.

--- Ex072.py
+++ Ex072.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado(entre 0 e 20) e mostrá-lo por extenso
+
 numext = ('zero', 'um', 'dois', 'três', 'quatro,',
           'cinco', 'seis', 'sete', 'oito', 'nove',
           'dez', 'onze', 'doze', 'treze', 'catorze',
