7ebdd370de007afdcd3d7fe8a0ca1e95d80c0c61
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 12:16:03 -0300

    Update Ex023.py
    Adicionado escopo do exercício.

--- Ex023.py
+++ Ex023.py
@@ -1,3 +1,5 @@
+#Desafio:Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+
 num = int(input('Digite um número entre 0 a 9999: '))
 u = num // 1 % 10
 d = num // 10 % 10
