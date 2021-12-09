e60ba7c063a444f479cc118b9fb63f3befef68d5
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:06:26 -0300

    Update Ex046.py
    Adicionado escopo do exercício.

--- Ex046.py
+++ Ex046.py
@@ -1,3 +1,6 @@
+#Desafio: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, 
+#indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
 from time import sleep
 print('=-' * 5, 'CONTAGEM REGRESSIVA ANO NOVO', '=-' * 5)
 
