96fe3863b07c2ba3a09b6ec24febb32885c886b6
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:49:16 -0300

    Update Ex100.py
    Adicionado escopo do exercício

--- Ex100.py
+++ Ex100.py
@@ -1,3 +1,6 @@
+#Desafio: Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somapar().
+#A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
+
 def sorteia(num):
     from random import randint
     from time import sleep
