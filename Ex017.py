496dd6fe60537825165277c06e6afb9567e94821
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 11:59:51 -0300

    Update Ex017.py
    Adicionado escopo do exercício.

--- Ex017.py
+++ Ex017.py
@@ -1,3 +1,6 @@
+#Desafio:Faça um programa que leia o comprimento de um cateto oposto e de um cateto adjacente de um triângulo retângulo,
+#calcule e mostre o comprimento da hiponetunsa.
+
 import math
 c1 = float(input('Valor do primeiro cateto: '))
 c2 = float(input('Valor do segundo cateto: '))
