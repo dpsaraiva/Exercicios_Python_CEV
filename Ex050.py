b3e65004ffecb5d584f1da3c68ca9b7e7b2c01b4
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:14:15 -0300

    Update Ex050.py
    Adicionado escopo do exercício.

--- Ex050.py
+++ Ex050.py
@@ -1,3 +1,6 @@
+#Desafio: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
+# Se o valor digitado for ímpar, desconsidere-o.
+
 soma = 0
 for c in range(1, 7):
     num = int(input('Digite um numero inteiro: '))
