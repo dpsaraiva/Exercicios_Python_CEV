4012f429f2864e8f095ecbf465328ad95a207dee
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 12:29:09 -0300

    Update Ex030.py
    Adicionado escopo do exercício.

--- Ex030.py
+++ Ex030.py
@@ -1,3 +1,5 @@
+#Desafio:Crie um programa que leia um número inteiro e mostre na tela se ele é "Par" ou "Ímpar".
+
 num = int(input('Digite um número: '))
 if num % 2 == 0:
     print('O número é par')
