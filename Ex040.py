4dd74599f1b66bdaa7f377f5a98135238c989bdd
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 15:58:03 -0300

    Update Ex040.py
    Adicionado escopo do exercício.

--- Ex040.py
+++ Ex040.py
@@ -1,3 +1,9 @@
+#Desafio: Crie um programa que leia duas notas de uma aluno e calcule sua média, 
+#mostrando um mensagem no final, de acordo com a media atingida
+# Média abaixo de 5.0: REPROVADO
+# Media entre 5.0 e 6.9: RECUPERAÇÃO
+# Media 7.0 ou superior: APROVADO
+
 nota1 = float(input('Digite sua primeira nota: '))
 nota2 = float(input('Digite sua segunda nota: '))
 media = (nota1 + nota2) / 2
