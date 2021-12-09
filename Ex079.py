89936874078c02a7e443ebdce9eaf2cd4bab9bbb
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:25:37 -0300

    Update Ex079.py
    Adicionado escopo do exercício

--- Ex079.py
+++ Ex079.py
@@ -1,3 +1,7 @@
+#Desafio: Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista.
+# Caso o número já exista la dentro, ele não será adicionado.
+# No final, serão exibidos todos os valores digitados, em ordem crescente.
+
 cadastro = list()
 while True:
     valor = int(input('Digite um valor: '))
