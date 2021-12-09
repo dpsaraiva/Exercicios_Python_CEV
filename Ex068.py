94aa050a37276fda8b7abbba8fe5101f727c423e
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:09:02 -0300

    Update Ex068.py
    Adicionado escopo do exercício.

--- Ex068.py
+++ Ex068.py
@@ -1,3 +1,6 @@
+#Desafio: Faça um programa que jogue par ou ímpar com o computador. O jogo será interrompido quando o jogador PERDER.
+# mostrando o total de vitórias consecutivas que ele conquistou no final do jogo
+
 from random import randint
 cont = 0
 while True:
