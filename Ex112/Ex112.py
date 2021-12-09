72267e4b0220f0830caf315c5d4391afc1672dba
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 18:10:28 -0300

    Update Ex112.py
    Adicionado escopo do exercício

--- Ex112/Ex112.py
+++ Ex112/Ex112.py
@@ -1,3 +1,7 @@
+#Desafio: Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado.
+#Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função input(), 
+#mas com uma validação de dados para aceitar apenas valores que sejam monetários.
+
 from utilidadecev import moedas, dados
 p = dados.leiaDinheiro('Digite o preço: R$ ')
 moedas.resumo(p, 80, 35)
