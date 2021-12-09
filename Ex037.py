e08de6ad1b876f734b8e858d0c925507b8484e3a
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 15:40:29 -0300

    Update Ex037.py
    Adicionado escopo do exercício.

--- Ex037.py
+++ Ex037.py
@@ -1,3 +1,9 @@
+#Desafio: Escreva um programa que leia um número inteiro qualquer e peça para o usuário
+#escolher qual será a base de conversão:
+#1 para binário;
+#2 para octal;
+#3 para hexadecimal.
+
 num = int(input('Digite um numero qualquer: '))
 print('Escolha a base de conversão:\n'
       'Digite 1 para binário;\n'
