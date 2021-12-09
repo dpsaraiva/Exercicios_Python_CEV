572022cea76ca7a0fb7a1e09a1e21b9644a46776
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:16:36 -0300

    Update Ex074.py
    Adicionado escopo do exercício.

--- Ex074.py
+++ Ex074.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa que vai gerar cinco números aleatórios e coloque em uma tupla.
+# Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla. 
+
 from random import randint
 a = randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10)
 maior = 0
