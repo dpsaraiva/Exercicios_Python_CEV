ae5f4bce65bf562c1390e613c7a40f9934dd6d3b
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 18:07:16 -0300

    Update Ex109.py
    Adicionado escopo do exercício

--- Ex109/Ex109.py
+++ Ex109/Ex109.py
@@ -1,3 +1,6 @@
+#Desafio: Modifique as funções que foram criadas no desafio 107 para que elas aceitem um parâmetro a mais,
+# informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
+
 import moeda
 
 p = float(input('Digite o preço: R$ '))
