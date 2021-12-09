1a3dc8e4a9ca463f7ca998c15622ccf59c9d7a3e
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:18:37 -0300

    Update Ex076.py
    Adicionado escopo do exercício.

--- Ex076.py
+++ Ex076.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+#No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+
 lista = ('Pão', 1.00, 'Leite', 4.00, 'Biscoito', 4.50, 'Macarrão', 3.50, 'Queijo', 30.00, 'Arroz', 30.00, 'Feijão', 20.00, 'Café', 12.00)
 print('-' * 51)
 print(f'{"LISTAGEM DE PREÇOS":^51}')
