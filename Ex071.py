2823b4492f167c69a5db892ed86e0d8c987bd75e
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:13:29 -0300

    Update Ex071.py
    Adicionado escopo do exercício.

--- Ex071.py
+++ Ex071.py
@@ -1,3 +1,7 @@
+#Desafio: Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário
+# qual será o valor a ser sacado(número inteiro) e o programa vai informar quantas cedulas de cada 
+# valor serão entregues. Obs: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1
+
 valor = int(input('Qual valor você quer sacar? '))
 nota50 = int(valor / 50)
 sobra50 = valor - nota50 * 50
