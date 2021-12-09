d11952f0c4dfb52d9d63f7fe4b889527674fb029
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:35:25 -0300

    Update Ex057.py
    Adicionado escopo do exercício.

--- Ex057.py
+++ Ex057.py
@@ -1,3 +1,6 @@
+#Desafio: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
+# Caso esteja errado, peça a digitação novamente até ter um valor correto. 
+
 sexo = input('Digite seu sexo no formato [M] ou [F]: ').strip().upper()[0]
 while sexo != 'F' and sexo != 'M':
     sexo = input('Valor invalído. Digite novamente: ').strip().upper()[0]
