3303eae81ab2bf0aad059155c301ffb8b654e6df
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:46:15 -0300

    Update Ex097.py
    Adicionado escopo do exercício

--- Ex097.py
+++ Ex097.py
@@ -1,3 +1,5 @@
+#Desafio: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável
+
 def escreva(texto):
     print('~' * (len(texto) + 4))
     print(f'  {texto}')
