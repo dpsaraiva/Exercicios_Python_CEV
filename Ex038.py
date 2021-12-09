685a760c13d8d07920e8b9c653c3c301b1f3425a
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 15:42:30 -0300

    Update Ex038.py
    Adicionado escopo do exercício.

--- Ex038.py
+++ Ex038.py
@@ -1,3 +1,9 @@
+#Desafio: Escreva um programa que leia dois números inteiros e compare-os mostrando na tela uma mensagem:
+#O primeiro valor é maior;
+#O segundo valor é maior;
+#Não existe valor maior, os dois são iguais.
+
+
 num1 = int(input('Digite o primeiro número: '))
 num2 = int(input('Digite o segundo número: '))
 if num1 > num2:
