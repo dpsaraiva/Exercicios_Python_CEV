c63f0e61a144d38306432c790245b557253f8588
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:59:08 -0300

    Update Ex105.py
    Adicionado escopo do exercício

--- Ex105.py
+++ Ex105.py
@@ -1,3 +1,11 @@
+#Desafio: Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
+# Quantidade de notas;
+# A maior nota;
+# A menor nota;
+# A média da turma;
+# A situação(opcional).
+# Adicione também as docstrings da função.
+
 def notas(*num, sit=False):
     '''
     -> Função para analisar notas e situação de vários alunos
