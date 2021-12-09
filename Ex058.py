860f09097a85f01ea04e55eea0229830fe5b468c
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:36:05 -0300

    Update Ex058.py
    Adicionado escopo do exercício.

--- Ex058.py
+++ Ex058.py
@@ -1,3 +1,6 @@
+#Desafio: Melhore o jogo do desafio 028 onde o computador vai pensar em um número entre 0 e 10.
+# Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer. 
+
 from random import randint
 num_pc = randint(0, 10)
 cont = 1
