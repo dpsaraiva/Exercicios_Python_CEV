f10e2392866c7721636f78ec9aafc3ac7d0ae527
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:24:53 -0300

    Update Ex078.py
    Adicionado escopo do exercício

--- Ex078.py
+++ Ex078.py
@@ -1,3 +1,6 @@
+#Desafio: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
+# No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+
 num = list()
 posmaior = list()
 for n in range(0, 5):
