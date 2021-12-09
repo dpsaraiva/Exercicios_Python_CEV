fbec9401837bef1415761ca58af20a466e8f5012
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:34:26 -0300

    Update Ex088.py
    Adicionado escopo do exercício

--- Ex088.py
+++ Ex088.py
@@ -1,3 +1,7 @@
+#Desafio: Faça um programa que ajude um jogador da MEGASENA a criar palpites.
+# O programa vai perguntar quantos jogos serão gerados e vai sortear 
+# 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+
 from random import sample
 from time import sleep
 jogos = int(input('Quantos jogos deseja gerar? '))
