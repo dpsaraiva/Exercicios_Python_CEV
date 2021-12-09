d325df344a37126a1b9c68a21dfa2fff9ca8a3d6
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 18:11:42 -0300

    Update Ex115.py
    Adicionado escopo do exercício

--- Ex115/Ex115.py
+++ Ex115/Ex115.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um pequeno sistema modularizado que permite cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples.
+#O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todas as pessoas cadastradas.
+
 from lib.interface import *
 from lib.arquivo import *
 from time import sleep
