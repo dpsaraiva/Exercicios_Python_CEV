e94301dbfee14b9aa0a20685787ca69c40259ba4
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 18:09:21 -0300

    Update Ex111.py
    Adicionado escopo do exercício

--- Ex111/Ex111.py
+++ Ex111/Ex111.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um pacote chamado utilidadesCeV que tenha dois módulos moeda e dado.
+#Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando.
+
 from utilidadecev import moedas
 p = float(input('Digite o preço: R$ '))
 moedas.resumo(p, 80, 35)
