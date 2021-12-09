22ffb886a5f72cb394de8fa0442a0e2a1ecdfb61
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:13:31 -0300

    Update Ex049.py
    Adicionado escopo do exercício.

--- Ex049.py
+++ Ex049.py
@@ -1,3 +1,5 @@
+#Desafio: Refaça o exercicio 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
 num = int(input('Digite um número inteiro: '))
 print('Sua tabuada é: ')
 for c in range(0, 11):
