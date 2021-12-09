973ee58abe1dcdffb0b579cb44088bd7681cd5e3
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:31:10 -0300

    Update Ex083.py
    Adicionado escopo do exercício

--- Ex083.py
+++ Ex083.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. 
+# Seu aplicativo deverá analisar se a expressão passada está com os parenteses abertos e fechados em ordem correta.
+
 num = list
 num = input('Digite a expressão: ')
 if num.count('(') == num.count(')'):
