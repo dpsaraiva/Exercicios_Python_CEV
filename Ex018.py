44dbb3602870d2fdb6937f7f1e267429de9f5a07
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 12:01:37 -0300

    Update Ex018.py
    Adicionado escopo do exercício.

--- Ex018.py
+++ Ex018.py
@@ -1,3 +1,5 @@
+#Desafio:Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
 import math
 ang = float(input('Digite o ângulo: '))
 sen = math.sin(math.radians(ang))
