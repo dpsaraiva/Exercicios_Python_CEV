bab76b8256752258e370a05f8d658878bb05d4e4
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:28:54 -0300

    Update Ex081.py
    Adicionado escopo do exercício

--- Ex081.py
+++ Ex081.py
@@ -1,3 +1,8 @@
+#Desafio: Crie um programa quue vai ler vários números e colocar em uma lista. Depois disso mostre:
+#A) Quantos números foram digitados.
+#B) A lista de valores, ordenada de forma decrescente.
+#C) Se o valor 5 foi digitado e está ou não na lista.
+
 num = list()
 while True:
     num.append(int(input('Digite um número: ')))
