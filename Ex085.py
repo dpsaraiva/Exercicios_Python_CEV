f2b8261326035e445d18ee7ad0d12658dc57e31f
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:32:28 -0300

    Update Ex085.py
    Adicionado escopo do exercício

--- Ex085.py
+++ Ex085.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa onde o usuário possa digitar 7 valores numéricos, e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
+# No final, mostre os valores pares e ímpares em ordem crescente.
+
 geral =[[], []]
 for i in range(1, 8):
     num = int(input(f'Digite o {i}º número: '))
