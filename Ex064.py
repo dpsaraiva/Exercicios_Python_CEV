33baa6ad354048a9c6c70e9eb483a5da10883c49
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:43:54 -0300

    Update Ex064.py
    Adicionado escopo do exercício.

--- Ex064.py
+++ Ex064.py
@@ -1,3 +1,7 @@
+#Desafio: Crie um program que leia vários números inteiros pelo teclado.
+#O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
+#No final, mostre quantos números foram digitados e qual foi a soma entre eles. 
+
 num = 0
 tot = 0
 cont = -1
