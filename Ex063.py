d58ce4ad1198b9bf525049a3fb5043b978bf34d9
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:39:35 -0300

    Update Ex063.py
    Adicionado escopo do exercício.

--- Ex063.py
+++ Ex063.py
@@ -1,3 +1,6 @@
+#Desafio: Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma Sequência de Fibonacci.
+# Ex: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
+
 num = int(input('Digite um número: '))
 termos = int(input('Quantos termos você quer? '))
 res = 0
