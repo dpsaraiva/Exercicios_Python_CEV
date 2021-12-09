63991e496e8c30eae7790eb3f8dd05fbd605d3e0
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:14:53 -0300

    Update Ex051.py
    Adicionado escopo do exercício.

--- Ex051.py
+++ Ex051.py
@@ -1,3 +1,6 @@
+#Desafio: Desenvolva um programa que leia o primeiro termo e a razão de uma P.A.
+# No final, mostre os 10 primeiros termos dessa progressão. 
+
 prim_termo = int(input('Qual o primeiro termo dessa dessa PA? '))
 razao = int(input('Qual a razão dessa PA? '))
 ult_termo = prim_termo + 10 * razao
