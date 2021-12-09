28652b14478af608552595ca29313fdb4c45fd7f
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:48:19 -0300

    Update Ex099.py
    Adicionado escopo do exercício

--- Ex099.py
+++ Ex099.py
@@ -1,3 +1,6 @@
+#Desafio: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
+#Seu programa tem que analizar todos os valores e dizer qual deles é maior.
+
 def maior(* num):
     from time import sleep
     print('=-' * 30)
