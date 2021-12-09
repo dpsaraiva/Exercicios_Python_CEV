6f37c4f7765c1382d65b720211d2ba763164742e
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 12:42:21 -0300

    Update Ex035.py
    Adicionado escopo do exercício.

--- Ex035.py
+++ Ex035.py
@@ -1,3 +1,5 @@
+#Desafio: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+
 a = int(input('Digite o primeiro valor: '))
 b = int(input('Digite o segundo valor: '))
 c = int(input('Digite o terceiro valor: '))
