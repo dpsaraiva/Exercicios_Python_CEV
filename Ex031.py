be0edb6ff8ba14b08a1551788b7a1d016f501dcb
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 12:35:23 -0300

    Update Ex031.py
    Adicionado escopo do exercício.

--- Ex031.py
+++ Ex031.py
@@ -1,3 +1,7 @@
+#Desafio: Desenvolva um programa que pergunte a distância de uma viagem em km.
+#Calcule o preço da passagem, cobrando R$ 0,50/km para viagens de ate 200 km
+# e R$ 0,45/km para viagens mais longas.
+
 dis = int(input('Qual a distância da viagem em km? '))
 if dis <= 200:
     valor = float(dis * 0.5)
