d50c97ede5cdcfeef28053c5b6b3a58aa8be47d7
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:37:21 -0300

    Update Ex060.py
    Adicionado escopo do exercício.

--- Ex060.py
+++ Ex060.py
@@ -1,3 +1,6 @@
+#Desafio: Faça um programa que leia um número qualquer e mostre seu fatorial.
+# Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120
+
 num = int(input('Digite um número inteiro: '))
 num1 = num
 res = num
