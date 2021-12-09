c776fa54bd7db090c597342519ede943c864787c
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 12:03:37 -0300

    Update Ex019.py
    Adicionado escopo do exercício.

--- Ex019.py
+++ Ex019.py
@@ -1,3 +1,6 @@
+#Desafio:Um professor quer sortear um de seus quatro alunos para apagar o quadro.
+#Faça um programa que ajude-o, lendo o nome deles e escrevendo o nome do escolhido.
+
 import random
 a = input('Nome do 1º aluno: ')
 b = input('Nome do 2º aluno: ')
