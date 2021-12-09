a5dcd3eb6aae0065be8863c18e09aab0188106f1
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 15:45:34 -0300

    Update Ex039.py
    Adicionado escopo do exercício.

--- Ex039.py
+++ Ex039.py
@@ -1,3 +1,9 @@
+#Desafio: Faça um programa que leia o ano de nascimento de um jovem e informe de acordo com sua idade:
+#Se ele ainda vai se alistar ao serviço militar;
+#Se é a hora dele se alistar;
+#Se ele ja passou do tempo de alistamento.
+#Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
 from datetime import date
 ano = int(input('Informe seu ano de nascimento com 4 digitos: ')) + 18
 ano_atual = date.today().year
