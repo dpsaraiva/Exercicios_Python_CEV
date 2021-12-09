587cb673c24ddbcf9b58a08620a9ab14be6174d1
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 12:21:24 -0300

    Update Ex026.py
    Adicionado escopo do exercício.

--- Ex026.py
+++ Ex026.py
@@ -1,3 +1,8 @@
+#Desafio:Faça um programa que leia uma frase pelo teclado e mostre:
+#Quantas vezes aparece a letra 'A'.
+#Em que posição ela aparece a primeira vez.
+#Em que posição ela aparece a última vez.
+
 frase = input('Escreva uma frase: ')
 frase = frase.upper()
 a = frase.count("A")
