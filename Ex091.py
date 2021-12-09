e764c409dd2f424f3cc95e9b8778ecdf3a41d46e
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:40:58 -0300

    Update Ex091.py
    Adicionado escopo do exercício

--- Ex091.py
+++ Ex091.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa ondde 4 jogadore joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário.
+# No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+
 from random import randint
 from operator import itemgetter
 from time import sleep
