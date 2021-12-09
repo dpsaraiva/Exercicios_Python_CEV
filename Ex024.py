f31b729ebc9b02ba4350748bc743259a80505490
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 12:17:51 -0300

    Update Ex024.py
    Adicionado escopo do exercício.

--- Ex024.py
+++ Ex024.py
@@ -1,3 +1,5 @@
+#Desafio:Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com nome 'Santo'.
+
 texto = input('Qual o nome da sua cidade? ')
 texto = texto.lstrip()
 texto = texto.capitalize()
