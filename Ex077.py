94d92b9d1b164d7143dafd089c51637d33b84415
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:20:17 -0300

    Update Ex077.py
    Adicionado escopo do exercício.

--- Ex077.py
+++ Ex077.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa que tenha uma tupla com várias palavras(não usar acentos).
+#Depois disso, você deve mostrar para cada palavra quais são as suas vogais.
+
 turpla = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR',
           'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
 pal = ''
