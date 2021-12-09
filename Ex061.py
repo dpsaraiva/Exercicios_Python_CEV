b9470996960d9e7edcad5c9f33197a01e2f4e3d4
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:37:58 -0300

    Update Ex061.py
    Adicionado escopo do exercício.

--- Ex061.py
+++ Ex061.py
@@ -1,3 +1,6 @@
+#Desafio: Refaça o desafio 51, lendo o primeiro termo e a razão de uma PA,
+#mostrando o 10 primeiros termos da progressão aritmética usando a estrutura while.
+
 prim_termo = int(input('Digite o 1º termo: '))
 razao = int(input('Qual a razão dessa PA? '))
 cont = 1
