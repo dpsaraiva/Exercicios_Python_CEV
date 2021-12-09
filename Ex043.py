215b998fbb20a1ec6c0121a3e0ebfd5b4a35a055
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 16:02:42 -0300

    Update Ex043.py
    Adicionado escopo do exercício.

--- Ex043.py
+++ Ex043.py
@@ -1,3 +1,10 @@
+#Desafio: Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:
+# Abaixo de 18.5: abaixo do peso
+# Entre 18.5 e 25: peso ideal
+# 25 até 30: sobrepeso
+# 30 até 40: Obesidade
+# acima de 40: Obesidade mórbida
+
 peso = float(input('Informe seu peso: '))
 altura = float(input('Informe sua altura: '))
 imc = peso / (altura * altura)
