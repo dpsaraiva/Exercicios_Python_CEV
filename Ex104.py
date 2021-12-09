a7fc817394a389595543467135f5b85af4c92e46
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:55:27 -0300

    Update Ex104.py
    Adicionado escopo do exercício

--- Ex104.py
+++ Ex104.py
@@ -1,3 +1,6 @@
+#Desafio: Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante a função input() do python,
+#só que fazendo a validação para aceitar apenas um valor numérico. Ex.: n = leiaInt("Digite um número:  ")
+
 def leiaInt(num):
     while True:
         num = input('Digite um número: ').strip()
