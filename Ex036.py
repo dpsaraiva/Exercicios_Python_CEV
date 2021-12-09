ec2370b86fb7f3d957579bef67637fb3b9244a01
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 15:36:51 -0300

    Update Ex036.py
    Adicionado escopo do exercício.

--- Ex036.py
+++ Ex036.py
@@ -1,3 +1,8 @@
+#Desafio: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+#O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+#Calcule o valor da prestação mensal sabendo que ele não pode exceder 30% do salario do comprador,
+#ou então o empréstimo será negado.
+
 print('=' * 20, '\033[1;31mCálculo Financiamento Bancário\033[m', '=' * 20)
 valor = float(input('Qual o valor da casa? '))
 salario = float(input('Qual o seu salário? '))
