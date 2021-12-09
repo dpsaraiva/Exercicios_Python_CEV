fb932170f28546b5412b3f5f6228d2455a3356f1
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:11:09 -0300

    Update Ex069.py
    Adicionado escopo do exercício.

--- Ex069.py
+++ Ex069.py
@@ -1,3 +1,9 @@
+#Desafio: Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada o programa deverá 
+# perguntar se o usuário quer ou não continuar. No final, mostre:
+# A) quantas pessoas tem mais de 18 anos.
+# B) Quantos homens foram cadastrados.
+# C) Quantas mulheres tem menos de 20 anos. 
+
 maior18 = mulheres = homens = 0
 while True:
     idade = int(input('Qual a sua idade? '))
