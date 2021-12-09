9f321c2fb743a40b6fdfd19cdeaede035f44c49c
Dionney Pereira Saraiva <80487374+dpsaraiva@users.noreply.github.com>
2021-12-09 17:56:25 -0300

    Update Ex096.py
    Adicionado escopo do exercício

--- Ex096.py
+++ Ex096.py
@@ -1,3 +1,5 @@
+#Desafio: Faça um programa que tenha uma função chamada area(), que receba as dimensões de um terreno retangular(largura e comprimento) e mostre a área do terreno.
+
 def area(larg, comp):
     res = larg * comp
     print(f'A área de um terreno {larg} x {comp} é {res}m²')
